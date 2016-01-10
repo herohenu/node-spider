710b6692a6156197ee894ffaa5c337269116aa84
Yinghao Lei <leiyinghao.bj@gmail.com>
2016-01-10 17:45:39 +0800

    get company info  success

--- index.js
+++ index.js
@@ -1,62 +1,60 @@
-//  get itjuzi   http://www.itjuzi.com/company/31592
-/*
-productName = $('div>span.title').text().trim();
-companyName = $('div.des-more span').text().trim();
-address = $('span.loca.c-gray-aset').text().trim();
-stage = $('td.mobile-none').text().trim();
-mainpage = $('a.weblink').attr('href');
-hirepage = '';
-*/
-
-//company  rule 1..1000  for each
-//
-
 var eventproxy = require('eventproxy');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
 var url = require('url');
+var async = require('async');
+var fs = require('fs');
 
 var itjuziUrl = 'http://www.itjuzi.com/company/';
+var companyUrls = [];
+for (i = 1; i <= 3; i++) {
+  var href = url.resolve(itjuziUrl, i.toString());  //
+  companyUrls.push(href);
+}
 
-superagent.get(itjuziUrl)
-	.end(function (err, res) {
-		if (err){
-			return console.err("   err is :", err);
-		}
-		console.log(" now get url  ",itjuziUrl);
-		var companyUrls = [];
-		var $ = cheerio.load(res.text);
-    for(i=1;i <=1000;i++){
-	    var href = url.resolve(itjuziUrl, i.toString());  //
-	    companyUrls.push(href);
-    }
-		console.log("companyUrls:\n", companyUrls);
-		var ep = new eventproxy();
-		ep.after('topic_html', companyUrls.length, function (topics) {
-			topics = topics.map(function (topicPair) {
-				var topicUrl = topicPair[0];
-				var topicHtml = topicPair[1];
-				var $ = cheerio.load(topicHtml);
-				return ({
-					titile: $('.topic_full_title').text().trim(),
-					href: topicUrl,
-					comment1: $('.reply_content').eq(0).text().trim()
-				});
-			});
-		
-			console.log("final topics :\n", topics);
-		
-		});
-		
-		companyUrls.forEach(function (topicUrl) {
-			superagent.get(topicUrl)
-				.end(function (err, res) {
-					console.log("   fetch url :", topicUrl);
-					ep.emit('topic_html', [topicUrl, res.text]);
-				})
-		});
+var concurrencyCount = 0;
+var fetchUrl = function (url, callback) {
+  companyurl = url;
+  concurrencyCount++;
+  superagent.get(companyurl)
+    .end(function (err, res) {
+      if (err) {
+        callback(err, 'fetch url:' + companyurl + ' err: ');
+        return console.log('fetch url:' + companyurl + ' err: ', err);
+      }
+      console.log('现在并发数目是******： ', concurrencyCount, ' **** fetch ' + companyurl + ' successful');
+      //ep.emit('topic_html', [companyurl, res.text]);
+      var $ = cheerio.load(res.text);
+      concurrencyCount--;
+      var cpobj = {
+        productName: $('div>span.title').text().replace(/[\n\t\r\s]/g, ""),
+        companyName: $('div.des-more>div> span').eq(0).text().replace(/[\n\t\r\s]/g, ""),
+        address: $('span.loca.c-gray-aset').text().replace(/[\n\t\r\s]/g, ""),
+        stage: $('td.mobile-none').text().replace(/[\n\t\r\s]/g, ""),
+        mainpage: $('a.weblink').attr('href'),
+        hirepage: ''
+      };
+      callback(null, cpobj);
+    });
+}
 
-	});
-
-//comment
-//如何存储到数据库或者写入文件
+async.mapLimit(companyUrls, 2, function (url, callback) {
+  console.log('url  is  :::', url);
+  fetchUrl(url, callback)
+}, function (err, result) {
+  console.log('now  err , result  is ', result);
+  if (!err) {
+    console.log('result.length  ', result);
+    arr = result;
+    var file = fs.createWriteStream('result.json');
+    file.on('error', function (err) {
+      console.log('write  file  error ', err);
+    });
+    console.log('print result  **********', '');
+    arr.forEach(function (v) {
+      //console.log('v is  ', JSON.stringify(v));
+      file.write(JSON.stringify(v) + ',\n');
+    });
+    file.end();
+  }
+})
--- store.js
+++ store.js
@@ -1,11 +1,17 @@
 // 将爬虫得到的数据存储到文件中
 var fs = require('fs');
-var obj ={
-	a :1,
-	b:4
+var obj = {
+  a: 1,
+  b: 4
 }
-fs.writeFile('aa.txt' , JSON.stringify(obj),function(err,result){
-	  if (err)  throw err;
-	  console.log(" it saved ");
 
-})
+var arr = [obj, obj]
+var file = fs.createWriteStream('array.txt');
+file.on('error', function (err) {
+  console.log('write  file  error ', err);
+});
+arr.forEach(function (v) {
+  console.log('v is  ', JSON.stringify(v) );
+  file.write(JSON.stringify(v)  +'\n');
+});
+file.end();
